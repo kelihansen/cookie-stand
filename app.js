568d27ac92f0a3c5b647990e99abf7572cc2cad7
Keli Hansen <keli.p.hansen@gmail.com>
2018-02-05 15:50:04 -0800

    added random number generator

--- app.js
+++ app.js
@@ -3,30 +3,45 @@
 const pdxAirportStore = {
     minCustPerHour: 23,
     maxCustPerHour: 65,
+    predictCustPerHour: function() {
+        return Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour;
+    },
     avgCookiesPerSale: 6.3,
 };
 
 const pioneerSquareStore = {
     minCustPerHour: 3,
     maxCustPerHour: 24,
+    predictCustPerHour: function() {
+        return Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour;
+    },
     avgCookiesPerSale: 1.2,
 };
 
 const powellsStore = {
     minCustPerHour: 11,
     maxCustPerHour: 38,
+    predictCustPerHour: function() {
+        return Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour;
+    },
     avgCookiesPerSale: 3.7,
 };
 
 const stJohnsStore = {
     minCustPerHour: 20,
     maxCustPerHour: 38,
+    predictCustPerHour: function() {
+        return Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour;
+    },
     avgCookiesPerSale: 2.3,
 };
 
 const waterfrontStore = {
     minCustPerHour: 2,
     maxCustPerHour: 16,
+    predictCustPerHour: function() {
+        return Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour;
+    },
     avgCookiesPerSale: 4.6,
 };
 
