fea8bfa24368b23910cec4327a8f182a9fe1e29e
Keli Hansen <keli.p.hansen@gmail.com>
2018-02-05 14:50:04 -0800

    created object literals with initial values

---
+++ app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const pdxAirportStore = {
+    minCustPerHour: 23,
+    maxCustPerHour: 65,
+    avgCookiesPerSale: 6.3,
+};
+
+const pioneerSquareStore = {
+    minCustPerHour: 3,
+    maxCustPerHour: 24,
+    avgCookiesPerSale: 1.2,
+};
+
+const powellsStore = {
+    minCustPerHour: 11,
+    maxCustPerHour: 38,
+    avgCookiesPerSale: 3.7,
+};
+
+const stJohnsStore = {
+    minCustPerHour: 20,
+    maxCustPerHour: 38,
+    avgCookiesPerSale: 2.3,
+};
+
+const waterfrontStore = {
+    minCustPerHour: 2,
+    maxCustPerHour: 16,
+    avgCookiesPerSale: 4.6,
+};
+
