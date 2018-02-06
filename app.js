adde96c3c817bc08625aa5d3a73ed15b5d8cc3bb
Keli Hansen <keli.p.hansen@gmail.com>
2018-02-06 13:18:01 -0800

    got constructor function working

--- app.js
+++ app.js
@@ -2,43 +2,25 @@
 
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-function listCookieSales(storeObject) {
-    const listSection = document.getElementById('store-lists');
-    const newHeading = document.createElement('h2');
-    listSection.appendChild(newHeading);
-    newHeading.textContent = storeObject.locationName;
-    const newUL = document.createElement('ul');
-    listSection.appendChild(newUL);
-    // added ID based on info from object, just to see how that might work:
-    // const storeID = storeObject.locationName.replace(/[^A-Za-z]/g, '').toLowerCase();
-    // newUL.setAttribute('id', storeID);
-    const dailyCookiesArray = storeObject.predictDailyCookies()[0];
-    for (let i = 0; i < dailyCookiesArray.length; i++) {
-        const newLI = document.createElement('li');
-        newUL.appendChild(newLI);
-        newLI.textContent = hours[i] + ': ' + dailyCookiesArray[i] + ' cookies';
-    }
-    const newLastLI = document.createElement('li');
-    newUL.appendChild(newLastLI);
-    newLastLI.textContent = 'Total: ' + storeObject.predictDailyCookies()[1] + ' cookies';
+function CookieStore (locationName, minCustPerHour, maxCustPerHour, avgCookiesPerSale) {
+    this.locationName = locationName;
+    this.minCustPerHour = minCustPerHour;
+    this.maxCustPerHour = maxCustPerHour;
+    this.avgCookiesPerSale = avgCookiesPerSale;
 }
 
-const pdxAirportStore = {
-    locationName: 'PDX Airport',
-    minCustPerHour: 23,
-    maxCustPerHour: 65,
-    avgCookiesPerSale: 6.3,
-    predictDailyCookies: function() {
-        const dailyCookiesArray = [];
-        let totalCookies = 0;
-        for (let i = 0; i < hours.length; i++) {
-            const custPerHourPrediction = Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour;
-            const cookiesPerHourPrediction = Math.round(custPerHourPrediction * this.avgCookiesPerSale);
-            dailyCookiesArray.push(cookiesPerHourPrediction);
-            totalCookies += dailyCookiesArray[i];
-        }
-        return [dailyCookiesArray, totalCookies];
+const pdxAirportStore = new CookieStore('PDX Airport', 23, 65, 6.3);
+
+CookieStore.prototype.predictDailyCookies = function() {
+    const dailyCookiesArray = [];
+    let totalCookies = 0;
+    for (let i = 0; i < hours.length; i++) {
+        const custPerHourPrediction = Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour;
+        const cookiesPerHourPrediction = Math.round(custPerHourPrediction * this.avgCookiesPerSale);
+        dailyCookiesArray.push(cookiesPerHourPrediction);
+        totalCookies += dailyCookiesArray[i];
     }
+    return [dailyCookiesArray, totalCookies];
 };
 
 const pioneerSquareStore = {
@@ -129,6 +111,28 @@ const waterfrontStore = {
     }
 };
 
+function listCookieSales(storeObject) {
+    const listSection = document.getElementById('store-lists');
+    const newHeading = document.createElement('h2');
+    listSection.appendChild(newHeading);
+    newHeading.textContent = storeObject.locationName;
+    const newUL = document.createElement('ul');
+    listSection.appendChild(newUL);
+    // added ID based on info from object, just to see how that might work:
+    // const storeID = storeObject.locationName.replace(/[^A-Za-z]/g, '').toLowerCase();
+    // newUL.setAttribute('id', storeID);
+    const dailyCookiesArray = storeObject.predictDailyCookies()[0];
+    for (let i = 0; i < dailyCookiesArray.length; i++) {
+        const newLI = document.createElement('li');
+        newUL.appendChild(newLI);
+        newLI.textContent = hours[i] + ': ' + dailyCookiesArray[i] + ' cookies';
+    }
+    const newLastLI = document.createElement('li');
+    newUL.appendChild(newLastLI);
+    newLastLI.textContent = 'Total: ' + storeObject.predictDailyCookies()[1] + ' cookies';
+}
+
+pdxAirportStore.predictDailyCookies();
 listCookieSales(pdxAirportStore);
 listCookieSales(pioneerSquareStore);
 listCookieSales(powellsStore);
