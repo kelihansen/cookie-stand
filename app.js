30ab8feedae14b9642a6b85cd9963e5b41428395
Keli Hansen <keli.p.hansen@gmail.com>
2018-02-05 18:19:15 -0800

    got lists displaying

--- app.js
+++ app.js
@@ -1,13 +1,55 @@
 'use strict';
 
+function add(a,b) {
+    return a + b;
+}
+
+const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
 const pdxAirportStore = {
+    locationName: 'PDX Airport',
     minCustPerHour: 23,
     maxCustPerHour: 65,
     predictCustPerHour: function() {
         return Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour;
     },
     avgCookiesPerSale: 6.3,
+    predictCookiesPerHour: function() {
+        return Math.round(this.predictCustPerHour() * this.avgCookiesPerSale);
+    },
+    dailyCookiesPrediction: function() {
+        const dailyCookiesArray = [];
+        let totalCookies = 0;
+        for (let i = 0; i < 14; i++) {
+            dailyCookiesArray.push(this.predictCookiesPerHour());
+            totalCookies = add(totalCookies, dailyCookiesArray[i]);
+        }
+        return [dailyCookiesArray, totalCookies];
+    }
+
 };
+
+function listCookieSales(storeObject) {
+    const listSection = document.getElementById('store-lists');
+    const newHeading = document.createElement('h2');
+    listSection.appendChild(newHeading);
+    newHeading.textContent = storeObject.locationName;
+    const newUL = document.createElement('ul');
+    listSection.appendChild(newUL);
+    const storeID = storeObject.locationName.replace(' ', '-');
+    newUL.setAttribute('id', storeID);
+    const storeList = document.getElementById(storeID);
+    for (let i = 0; i < storeObject.dailyCookiesPrediction()[0].length; i++) {
+        const newLI = document.createElement('li');
+        storeList.appendChild(newLI);
+        newLI.textContent = hours[i] + ': ' + storeObject.dailyCookiesPrediction()[0][i] + ' cookies';
+    }
+    const newLI = document.createElement('li');
+    storeList.appendChild(newLI);
+    newLI.textContent = 'Total: ' + storeObject.dailyCookiesPrediction()[1] + ' cookies';
+}
+
+listCookieSales(pdxAirportStore);
 
 const pioneerSquareStore = {
     minCustPerHour: 3,
@@ -15,7 +57,7 @@ const pioneerSquareStore = {
     predictCustPerHour: function() {
         return Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour;
     },
-    avgCookiesPerSale: 1.2,
+    avgCookiesPerSale: 1.2
 };
 
 const powellsStore = {
@@ -24,7 +66,7 @@ const powellsStore = {
     predictCustPerHour: function() {
         return Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour;
     },
-    avgCookiesPerSale: 3.7,
+    avgCookiesPerSale: 3.7
 };
 
 const stJohnsStore = {
@@ -33,7 +75,7 @@ const stJohnsStore = {
     predictCustPerHour: function() {
         return Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour;
     },
-    avgCookiesPerSale: 2.3,
+    avgCookiesPerSale: 2.3
 };
 
 const waterfrontStore = {
@@ -42,6 +84,5 @@ const waterfrontStore = {
     predictCustPerHour: function() {
         return Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour;
     },
-    avgCookiesPerSale: 4.6,
+    avgCookiesPerSale: 4.6
 };
-
