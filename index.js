bc99b7beb06570f34c7b26bae3fed96986ddf009
Keli Hansen <keli.p.hansen@gmail.com>
2018-02-11 20:17:26 -0800

    added and positioned hero image

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// const logo = document.getElementById('logo');
+// const header = document.getElementsByTagName('header')[0];
+
+// window.addEventListener('scroll', function() {
+//     const scrollDistance = window.scrollY;
+//     if (scrollDistance >= logo.offsetTop) {
+//         header.classList.add('fix-header');
+//     } else {
+//         header.classList.remove('fix-header');
+//     }
+// });
