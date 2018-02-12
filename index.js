24ca50bbf0d3f84697c200c41df11251ecc6aa54
Keli Hansen <keli.p.hansen@gmail.com>
2018-02-12 03:39:32 -0800

    got modal images working

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 'use strict';
 
-// const logo = document.getElementById('logo');
-// const header = document.getElementsByTagName('header')[0];
+const firstLink = document.getElementById('open-first-cookie');
+const firstCookiePicDiv = document.getElementById('first-cookie');
+const firstCookiePic = document.querySelector('#first-cookie img');
+const firstCloseButton = document.querySelector('#first-cookie .close-x');
 
-// window.addEventListener('scroll', function() {
-//     const scrollDistance = window.scrollY;
-//     if (scrollDistance >= logo.offsetTop) {
-//         header.classList.add('fix-header');
-//     } else {
-//         header.classList.remove('fix-header');
-//     }
-// });
+firstLink.addEventListener('click', function() {
+    event.preventDefault();
+    firstCookiePicDiv.classList.add('modal');
+    firstCookiePic.classList.remove('closed');
+    firstCookiePic.classList.add('open');
+});
+
+firstCloseButton.addEventListener('click', function() {
+    event.preventDefault();
+    firstCookiePicDiv.classList.remove('modal');
+    firstCookiePic.classList.remove('open');
+    firstCookiePic.classList.add('closed');
+});
+
+const secondLink = document.getElementById('open-better-cookies');
+const secondCookiePicDiv = document.getElementById('better-cookies');
+const secondCookiePic = document.querySelector('#better-cookies img');
+const secondCloseButton = document.querySelector('#better-cookies .close-x');
+
+secondLink.addEventListener('click', function() {
+    event.preventDefault();
+    secondCookiePicDiv.classList.add('modal');
+    secondCookiePic.classList.remove('closed');
+    secondCookiePic.classList.add('open');
+});
+
+secondCloseButton.addEventListener('click', function() {
+    event.preventDefault();
+    secondCookiePicDiv.classList.remove('modal');
+    secondCookiePic.classList.remove('open');
+    secondCookiePic.classList.add('closed');
+});
+
+
